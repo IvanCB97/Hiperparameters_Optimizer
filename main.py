4a7ebffeed7b1e935ff2c45be0433a033c2b5f10
Iván <ivancastrobernaza@gmail.com>
2023-06-08 17:33:59 +0200

    CREACIÓN DEL REPOSITORIO

---
+++ main.py
@@ -0,0 +1,8 @@
+
+"""Archivo que contiene la ejecucion principal del programa"""
+
+from menu import menu_principal
+
+
+if __name__ == '__main__':                            #aqui se comienza a ejecutar
+    menu_principal()                                  #ejecutamos menu_principal
